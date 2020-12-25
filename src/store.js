cc0b00724f1639632c7d311bb1d8001f63319cda
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2020-12-25 18:33:01 -0300

    part-4-store

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducer'
+import { print1, print2, print3 } from './exampleAddons/middleware'
+
+const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+
+// Pass enhancer as the second arg, since there's no preloadedState
+const store = createStore(rootReducer, middlewareEnhancer)
+
+export default store
